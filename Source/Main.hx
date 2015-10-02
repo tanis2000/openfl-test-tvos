ae7c8a840627252cf5736d68b575237bd6b58f0e
Valerio Santinelli <santinelli@altralogica.it>
2015-10-02 17:18:09 +0200

    Added some debug stuff

--- Source/Main.hx
+++ Source/Main.hx
@@ -4,6 +4,10 @@ package;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.display.Sprite;
+import openfl.display.Graphics;
+import openfl.display.CapsStyle;
+import openfl.display.JointStyle;
+import openfl.display.LineScaleMode;
 import openfl.Assets;
 
 
@@ -15,12 +19,21 @@ class Main extends Sprite {
 		super ();
 		
 		var bitmap = new Bitmap (Assets.getBitmapData ("assets/openfl.png"));
+		trace(bitmap);
+		trace(bitmap.width);
+		trace(bitmap.height);
 		addChild (bitmap);
 		
 		bitmap.x = (stage.stageWidth - bitmap.width) / 2;
 		bitmap.y = (stage.stageHeight - bitmap.height) / 2;
-		
+		/*
+		this.graphics.lineStyle(10, 0xFF0000, 1, LineScaleMode.NORMAL, CapsStyle.NONE, JointStyle.ROUND, 10);
+		this.graphics.beginFill(0x00FF00, 1);
+		this.graphics.drawRect(0, 0, 500, 500);
+		this.graphics.endFill();
+		*/
 	}
+
 	
 	
 }
