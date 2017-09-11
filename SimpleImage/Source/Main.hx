ea35e90b3e9dd97375d0f2a0088233036e3c3a55
Valerio Santinelli <santinelli@altralogica.it>
2017-09-11 14:47:26 +0200

    Updated to the latest Lime

--- SimpleImage/Source/Main.hx
+++ SimpleImage/Source/Main.hx
@@ -13,8 +13,8 @@ import lime.Assets;
 
 
 class Main extends Application {
-
-
+	
+	
 	private var cairoSurface:CairoSurface;
 	private var glBuffer:GLBuffer;
 	private var glMatrixUniform:GLUniformLocation;
@@ -23,107 +23,109 @@ class Main extends Application {
 	private var glTextureAttribute:Int;
 	private var glVertexAttribute:Int;
 	private var image:Image;
-
-
+	
+	
 	public function new () {
-
+		
 		super ();
-
+		
 	}
-
-
+	
+	
 	public override function render (renderer:Renderer):Void {
-
+		
 		if (image == null && preloader.complete) {
-
+			
 			image = Assets.getImage ("assets/lime.png");
-
+			
 			switch (renderer.context) {
-
+				
 				case CAIRO (cairo):
-
+					
 					image.format = BGRA32;
 					image.premultiplied = true;
-
+					
 					cairoSurface = CairoImageSurface.fromImage (image);
-
+				
 				case CANVAS (context):
-
+					
 					context.fillStyle = "#" + StringTools.hex (config.windows[0].background, 6);
 					context.fillRect (0, 0, window.width, window.height);
 					context.drawImage (image.src, 0, 0, image.width, image.height);
-
+				
 				case DOM (element):
-
+					
 					element.style.backgroundColor = "#" + StringTools.hex (config.windows[0].background, 6);
 					element.appendChild (image.src);
-
+				
 				case FLASH (sprite):
-
+					
 					#if flash
 					var bitmap = new flash.display.Bitmap (image.src);
 					sprite.addChild (bitmap);
 					#end
-
+				
 				case OPENGL (gl):
-
-					var vertexSource =
-
+					
+					var gl:WebGLContext = gl;
+					
+					var vertexSource = 
+						
 						"attribute vec4 aPosition;
 						attribute vec2 aTexCoord;
 						varying vec2 vTexCoord;
-
+						
 						uniform mat4 uMatrix;
-
+						
 						void main(void) {
-
+							
 							vTexCoord = aTexCoord;
 							gl_Position = uMatrix * aPosition;
-
+							
 						}";
-
-					var fragmentSource =
-
+					
+					var fragmentSource = 
+						
 						#if !desktop
 						"precision mediump float;" +
 						#end
 						"varying vec2 vTexCoord;
 						uniform sampler2D uImage0;
-
+						
 						void main(void)
 						{
 							gl_FragColor = texture2D (uImage0, vTexCoord);
 						}";
-
+					
 					glProgram = GLUtils.createProgram (vertexSource, fragmentSource);
 					gl.useProgram (glProgram);
-
+					
 					glVertexAttribute = gl.getAttribLocation (glProgram, "aPosition");
 					glTextureAttribute = gl.getAttribLocation (glProgram, "aTexCoord");
 					glMatrixUniform = gl.getUniformLocation (glProgram, "uMatrix");
 					var imageUniform = gl.getUniformLocation (glProgram, "uImage0");
-
+					
 					gl.enableVertexAttribArray (glVertexAttribute);
 					gl.enableVertexAttribArray (glTextureAttribute);
 					gl.uniform1i (imageUniform, 0);
-
+					
 					gl.blendFunc (gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 					gl.enable (gl.BLEND);
-
+					
 					var data = [
-
-						window.width, window.height, 0, 1, 1,
-						0, window.height, 0, 0, 1,
-						window.width, 0, 0, 1, 0,
+						
+						image.width, image.height, 0, 1, 1,
+						0, image.height, 0, 0, 1,
+						image.width, 0, 0, 1, 0,
 						0, 0, 0, 0, 0
-
+						
 					];
-
+					
 					glBuffer = gl.createBuffer ();
 					gl.bindBuffer (gl.ARRAY_BUFFER, glBuffer);
 					gl.bufferData (gl.ARRAY_BUFFER, new Float32Array (data), gl.STATIC_DRAW);
 					gl.bindBuffer (gl.ARRAY_BUFFER, null);
-
+					
 					glTexture = gl.createTexture ();
 					gl.bindTexture (gl.TEXTURE_2D, glTexture);
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -136,65 +138,70 @@ class Main extends Application {
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 					gl.texParameteri (gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 					gl.bindTexture (gl.TEXTURE_2D, null);
-
+				
 				default:
-
+				
 			}
-
+			
 		}
-
+		
 		switch (renderer.context) {
-
+			
 			case CAIRO (cairo):
-
+				
 				var r = ((config.windows[0].background >> 16) & 0xFF) / 0xFF;
 				var g = ((config.windows[0].background >> 8) & 0xFF) / 0xFF;
 				var b = (config.windows[0].background & 0xFF) / 0xFF;
 				var a = ((config.windows[0].background >> 24) & 0xFF) / 0xFF;
-
+				
 				cairo.setSourceRGB (r, g, b);
 				cairo.paint ();
-
+				
 				image.format = BGRA32;
 				image.premultiplied = true;
-
+				
 				cairo.setSourceSurface (cairoSurface, 0, 0);
 				cairo.paint ();
-
+			
 			case OPENGL (gl):
-
-				trace(window.width);
+				
+				var gl:WebGLContext = gl;
+				
 				gl.viewport (0, 0, window.width, window.height);
-
+				
 				var r = ((config.windows[0].background >> 16) & 0xFF) / 0xFF;
 				var g = ((config.windows[0].background >> 8) & 0xFF) / 0xFF;
 				var b = (config.windows[0].background & 0xFF) / 0xFF;
 				var a = ((config.windows[0].background >> 24) & 0xFF) / 0xFF;
-
+				
 				gl.clearColor (r, g, b, a);
 				gl.clear (gl.COLOR_BUFFER_BIT);
-
-				var matrix = Matrix4.createOrtho (0, window.width, window.height, 0, -1000, 1000);
-				gl.uniformMatrix4fv (glMatrixUniform, false, matrix);
-
-				gl.activeTexture (gl.TEXTURE0);
-				gl.bindTexture (gl.TEXTURE_2D, glTexture);
-
-				#if desktop
-				gl.enable (gl.TEXTURE_2D);
-				#end
-
-				gl.bindBuffer (gl.ARRAY_BUFFER, glBuffer);
-				gl.vertexAttribPointer (glVertexAttribute, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
-				gl.vertexAttribPointer (glTextureAttribute, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
-
-				gl.drawArrays (gl.TRIANGLE_STRIP, 0, 4);
-
+				
+				if (image != null) {
+					
+					var matrix = Matrix4.createOrtho (0, window.width, window.height, 0, -1000, 1000);
+					gl.uniformMatrix4fv (glMatrixUniform, false, matrix);
+					
+					gl.activeTexture (gl.TEXTURE0);
+					gl.bindTexture (gl.TEXTURE_2D, glTexture);
+					
+					#if desktop
+					gl.enable (gl.TEXTURE_2D);
+					#end
+					
+					gl.bindBuffer (gl.ARRAY_BUFFER, glBuffer);
+					gl.vertexAttribPointer (glVertexAttribute, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+					gl.vertexAttribPointer (glTextureAttribute, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+					
+					gl.drawArrays (gl.TRIANGLE_STRIP, 0, 4);
+					
+				}
+				
 			default:
-
+			
 		}
-
+		
 	}
-
-
+	
+	
 }
